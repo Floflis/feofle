2eb982185917976dec15a6439cd1b68f84c13f4f
Daniella Mesquita <mesquitadaniella@proton.me>
2022-10-17 16:51:49 -0300

    Complement previous commit

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo "Welcome to the Floflis People installer!"
+echo "Welcome to the Floflis' Feofle installer!"
 
 echo "- Copying (probably) a lot of files. Don't worry if it takes several times..."
-sudo mkdir /usr/lib/people
-sudo cp -r -f --preserve=all . /usr/lib/people
+sudo mkdir /usr/lib/feofle
+sudo cp -r -f --preserve=all . /usr/lib/feofle
 
-echo "- Installing Floflis People in /usr/bin..."
-sudo cat > /usr/bin/people << ENDOFFILE
+echo "- Installing Floflis' Feofle in /usr/bin..."
+sudo cat > /usr/bin/feofle << ENDOFFILE
 #!/bin/bash
 
-source /usr/lib/people/people
+source /usr/lib/feofle/feofle
 ENDOFFILE
 
-echo "- Turning Floflis People into a executable..."
-chmod 755 /usr/bin/people && sudo chmod +x /usr/bin/people
+echo "- Turning Floflis' Feofle into a executable..."
+chmod 755 /usr/bin/feofle && sudo chmod +x /usr/bin/feofle
 
-sudo rm /usr/lib/people/install.sh # no need anymore to use the installer again
+sudo rm /usr/lib/feofle/install.sh # no need anymore to use the installer again
 
 echo "Done!"
